1f29fd4de59287fd2e670c39efbadf2ba75e9acd
draganasavanovic <dragana.savanovic@wayseven.com>
2020-02-21 12:03:22 +0100

    button and icon material component

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
+import { MatButtonModule } from "@angular/material/button";
+import { MatInputModule } from "@angular/material/input";
+import { MatIconModule } from "@angular/material/icon";
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule],
-  exports: []
+  imports: [MatButtonModule, MatInputModule, MatIconModule],
+  exports: [MatButtonModule, MatInputModule, MatIconModule]
 })
 export class MaterialModule {}
