2604e92e15f091ab9b3c9e2008afb4e32ad6c410
draganasavanovic <dragana.savanovic@wayseven.com>
2020-02-27 13:06:22 +0100

    created welcome component, added routing, created app structure, flex layout, hints and validation errors, datetime picker, restricting pickable dates

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,10 +2,27 @@ import { NgModule } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
 import { MatInputModule } from "@angular/material/input";
 import { MatIconModule } from "@angular/material/icon";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatNativeDateModule } from "@angular/material/core";
 
 @NgModule({
   declarations: [],
-  imports: [MatButtonModule, MatInputModule, MatIconModule],
-  exports: [MatButtonModule, MatInputModule, MatIconModule]
+  imports: [
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+  ]
 })
 export class MaterialModule {}
