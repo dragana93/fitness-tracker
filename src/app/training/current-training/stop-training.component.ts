f8994ae2632452b96a20c23efeacc449d6bdb0aa
draganasavanovic <dragana.savanovic@wayseven.com>
2020-03-11 09:24:17 +0100

    passed data to the dialog

--- src/app/training/current-training/stop-training.component.ts
+++ src/app/training/current-training/stop-training.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit } from "@angular/core";
-import { MatDialog } from "@angular/material/dialog";
+import { Component, OnInit, Inject } from "@angular/core";
+import { MAT_DIALOG_DATA } from "@angular/material/dialog";
 
 @Component({
   selector: "app-stop-training",
   template: `
     <h1 mat-dialog-title>Are you sure?</h1>
+    <mat-dialog-content>
+      You already got {{ passedData.progress }} %
+    </mat-dialog-content>
     <mat-dialog-actions>
       <button mat-button [mat-dialog-close]="true">Yes</button>
       <button mat-button [mat-dialog-close]="false">No</button>
@@ -13,7 +16,7 @@ import { MatDialog } from "@angular/material/dialog";
   styleUrls: ["./current-training.component.css"]
 })
 export class StopTrainingComponent implements OnInit {
-  constructor(private dialog: MatDialog) {}
+  constructor(@Inject(MAT_DIALOG_DATA) public passedData: any) {}
 
   ngOnInit(): void {}
 }
