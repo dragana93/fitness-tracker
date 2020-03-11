4727a4cde41cc356ecdb56b0539ef1fd8714719e
draganasavanovic <dragana.savanovic@wayseven.com>
2020-03-11 14:10:04 +0100

    prepared the exercise data

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -12,6 +12,7 @@ import { AuthService } from "./auth.service";
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
+
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     if (this.authService.isAuth()) {
       return true;
