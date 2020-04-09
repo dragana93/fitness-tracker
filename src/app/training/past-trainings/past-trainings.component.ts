400e0e19b166a11072e8a0403f9edf1e677b5c85
draganasavanovic <dragana.savanovic@wayseven.com>
2020-04-09 10:11:32 +0200

    connecting the data table

--- src/app/training/past-trainings/past-trainings.component.ts
+++ src/app/training/past-trainings/past-trainings.component.ts
@@ -1,10 +1,4 @@
-import {
-  Component,
-  OnInit,
-  ViewChild,
-  AfterViewInit,
-  OnDestroy
-} from "@angular/core";
+import { Component, OnInit, ViewChild, AfterViewInit } from "@angular/core";
 import { MatTableDataSource } from "@angular/material/table";
 import { Exercise } from "../exercise.model";
 import { TrainingService } from "../training.service";
@@ -12,29 +6,34 @@ import { MatSort } from "@angular/material/sort";
 import { MatPaginator } from "@angular/material/paginator";
 import { Subscription } from "rxjs";
 
+import { Store } from "@ngrx/store";
+import * as formTraining from "../training.reducer";
+
 @Component({
   selector: "app-past-trainings",
   templateUrl: "./past-trainings.component.html",
-  styleUrls: ["./past-trainings.component.css"]
+  styleUrls: ["./past-trainings.component.css"],
 })
-export class PastTrainingsComponent
-  implements OnInit, AfterViewInit, OnDestroy {
+export class PastTrainingsComponent implements OnInit, AfterViewInit {
   displayedColumns = ["date", "name", "calories", "duration", "state"];
   dataSource = new MatTableDataSource<Exercise>();
 
-  private exChangedSubscription: Subscription;
+  // private exChangedSubscription: Subscription;
 
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  constructor(private trainingService: TrainingService) {}
+  constructor(
+    private trainingService: TrainingService,
+    private store: Store<formTraining.State>
+  ) {}
 
   ngOnInit(): void {
-    this.exChangedSubscription = this.trainingService.finishedExercisesChanged.subscribe(
-      (exercises: Exercise[]) => {
+    this.store
+      .select(formTraining.getFinishedExercises)
+      .subscribe((exercises: Exercise[]) => {
         this.dataSource.data = exercises;
-      }
-    );
+      });
     this.trainingService.fatchCompletedOrCancelledExercises();
   }
 
@@ -43,11 +42,11 @@ export class PastTrainingsComponent
     this.dataSource.paginator = this.paginator;
   }
 
-  ngOnDestroy() {
-    if (this.exChangedSubscription) {
-      this.exChangedSubscription.unsubscribe();
-    }
-  }
+  // ngOnDestroy() {
+  //   if (this.exChangedSubscription) {
+  //     this.exChangedSubscription.unsubscribe();
+  //   }
+  // }
 
   doFilter(filterValue: string) {
     this.dataSource.filter = filterValue.trim().toLowerCase();
